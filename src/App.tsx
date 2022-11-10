58b935fc0c23fff9d4d007d08f386c60673f306e
wibus-wee <1596355173@qq.com>
2022-11-11 00:24:29 +0800

    feat: `map()` output items

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import { Item } from './components/item'
 import './main.css'
+import { generateLessonsList } from './utils/lesson'
 
 function App() {
+
+  const [data, setData] = useState(generateLessonsList())
+  console.log(data)
 
   return (
     <div className={"wrapper"}>
@@ -12,13 +16,21 @@ function App() {
           <span>每 20 分钟刷新 &nbsp;&nbsp;&nbsp; By Wibus.</span>
         </h3>
       </div>
-      <Item 
-        index={1}
-        name={"计算机网络"}
-        id={"123456789"}
-        password={"123456"}
-        prefix={"可能的"}
-      />
+
+      {
+        Object.keys(data).map((key, index) => {
+          const PREFIX = index === 0 ? "现在的" : index === 1 ? "下一节" : index === 2 ? "下下节" : "下下下节"
+          console.log(data[key])
+          return (
+            <Item
+              index={data[key].time.index}
+              name={data[key].name}
+              prefix={`${PREFIX}`}
+            />
+          )
+        })
+      }
+
     </div>
   )
 }
--- src/components/item.tsx
+++ src/components/item.tsx
@@ -3,21 +3,23 @@
  * @author: Wibus
  * @Date: 2022-11-10 23:34:25
  * @LastEditors: Wibus
- * @LastEditTime: 2022-11-10 23:50:05
+ * @LastEditTime: 2022-11-11 00:17:16
  * Coding With IU
  */
 
+import { lessons } from "../constants/lessonsInfo";
 import { insert } from "../utils/insert";
 import { lessonsTime } from "../utils/lesson";
 
 export const Item = (props: {
-  index: number;
-  name: string;
-  id: string;
-  password: string;
-  prefix: string;
+  index: number; // 第几节课
+  name: string; // 课程名
+  prefix: string; // 课程前缀
 }) => {
-  const dumpid = props.id.replace(/-/g, "");
+  const name = props.name as keyof typeof lessons;
+  const lesson = lessons[name];
+  if (!lesson) return <div></div>;
+  const dumpid = lesson.id?.replace(/-/g, "") || "0000000000";
   const startTime = insert(String(lessonsTime[props.index - 1].start), 2, ":");
   const endTime = insert(String(lessonsTime[props.index - 1].end), 2, ":");
   return (
@@ -25,11 +27,11 @@ export const Item = (props: {
       <a
         href={`wemeet://page/inmeeting?meeting_code=${dumpid}`}
         onClick={() => {
-          navigator.clipboard.writeText(props.password);
+          navigator.clipboard.writeText(lesson.password);
         }} className="appItem">
         <div style={{ position: "relative" }}>
-          <h5>{props.prefix}课: {props.index} {props.name} &nbsp;&nbsp; {`${startTime} - ${endTime}`}</h5>
-          <p>会议号：{props.id}</p>
+          <h5>{props.prefix}课: {props.index} {name} &nbsp;&nbsp; {`${startTime} - ${endTime}`}</h5>
+          <p>会议号：{lesson?.id || "null"}</p>
         </div>
       </a>
     </div>

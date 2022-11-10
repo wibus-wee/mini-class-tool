1086d29650e33d3e4ab0d9cf606867369f7789f1
wibus-wee <1596355173@qq.com>
2022-11-11 00:24:18 +0800

    feat: generate lessons list

--- src/utils/lesson.ts
+++ src/utils/lesson.ts
@@ -70,24 +70,43 @@ function getTodayLessonList() {
 }
 
 /**
+ * 如果当前没有课程，将会返回 null
  * @returns 当前时间对应的课程，以及预估接下来的课程
  */
 export function generateLessons(number: number = 1) {
-  const Time = Number(getTime()) // 获取当前时间
+  const date = new Date();
+  const day = date.getDay(); // 今天是周几
+  // const Time = Number(getTime()) // 获取当前时间
+  const Time = 1020;
   const Today = getTodayLessonList() // 获取今天的课程列表
-  const NowLessonIndex = lessonsTime.find(item => { // 查找当前时间对应的课程
-    return Time >= item.start && Time <= item.end // 当前时间在课程时间范围内的
-  })!.index
+  const NowLessonIndex = lessonsTime.find(item => { // 查找当前时间对应的课程时间范围
+    return Time >= item.start && Time <= item.end
+  })?.index
+
+  if (!NowLessonIndex) return null // 如果当前没有课程，将会返回 null
 
   const Lessons = Object();
   for (let i = 0; i < number; i++) {
     Lessons[i] = Today[NowLessonIndex + i - 1]
   }
 
-  const NowLesson = Today[NowLessonIndex - 1] // 获取当前课程
-  const NextLesson = Today[NowLessonIndex] // 获取下一节课
-  const Next2Lesson = Today[NowLessonIndex + 1] // 获取下下节课
-  const Next3Lesson = Today[NowLessonIndex + 2] // 获取下下下节课
+  const NowLesson = {
+    name: Today[NowLessonIndex - 1],
+    index: NowLessonIndex - 1,
+  } // 获取当前课程
+  const NextLesson = {
+    name: Today[NowLessonIndex],
+    index: NowLessonIndex,
+  } // 获取下一节课
+  const Next2Lesson = {
+    name: Today[NowLessonIndex + 1],
+    index: NowLessonIndex + 1,
+  } // 获取下下节课
+  const Next3Lesson = {
+    name: Today[NowLessonIndex + 2],
+    index: NowLessonIndex + 2,
+
+  } // 获取下下下节课
 
   return {
     NowLesson,
@@ -97,3 +116,35 @@ export function generateLessons(number: number = 1) {
   }
 
 } // 选择课程
+
+export const generateLessonsList: (number?: number) => {
+  [key: string]: {
+    name: string,
+    time: {
+      index: number;
+      start: string | number;
+      end: string | number;
+    },
+  }
+} = (number: number = 1) => {
+  const Lessons = generateLessons(number)
+  if (!Lessons) return {}
+  let res = Object();
+  res.now = {
+    name: Lessons?.NowLesson.name,
+    time: lessonsTime[Lessons.NowLesson.index - 1]
+  }
+  res.next = {
+    name: Lessons?.NextLesson.name,
+    time: lessonsTime[Lessons.NextLesson.index - 1]
+  }
+  res.next2 = {
+    name: Lessons?.Next2Lesson.name,
+    time: lessonsTime[Lessons.Next2Lesson.index - 1]
+  }
+  res.next3 = {
+    name: Lessons?.Next3Lesson.name,
+    time: lessonsTime[Lessons.Next3Lesson.index - 1]
+  }
+  return res
+}

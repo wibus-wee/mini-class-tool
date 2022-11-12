d0e46e715c7b98f4911c1ffd833b9d68ab731866
wibus-wee <1596355173@qq.com>
2022-11-12 23:59:47 +0800

    fix: default value

--- src/constants/lessonsInfo.ts
+++ src/constants/lessonsInfo.ts
@@ -3,7 +3,7 @@
  * @author: Wibus
  * @Date: 2022-11-10 07:37:58
  * @LastEditors: Wibus
- * @LastEditTime: 2022-11-11 15:09:48
+ * @LastEditTime: 2022-11-12 23:59:24
  * Coding With IU
  */
 
@@ -13,50 +13,50 @@
 export const lessons = {
   '语文': {
     id: '767-3951-1565',
-    password: '1717',
+    password: '',
   },
   '数学': {
     id: '3653-9899-668',
-    password: '2022',
+    password: '',
   },
   '英语': {
     id: '745-5493-2732',
-    password: '202211',
+    password: '',
   },
   '自习': {
     id: '974-7442-1711',
-    password: '98765',
+    password: '',
   },
   '物理': {
     id: '775-8722-1444',
-    password: '171717',
+    password: '',
   },
   '化学': {
     id: '547-2138-0070',
-    password: '220203',
+    password: '',
   },
   '政治': {
     id: '382-9587-9578',
-    password: '202406',
+    password: '',
   },
   '通用技术': {
     id: '595-6282-9928',
-    password: '314119',
+    password: '',
   },
   '体育': {
     id: '896-9889-4763',
-    password: '202217',
+    password: '',
   },
   '班会': {
     id: '974-7442-1711',
-    password: '98765',
+    password: '',
   },
   '团队': {
     id: '974-7442-1711',
-    password: '98765',
+    password: '',
   },
-  '睡觉': {
+  '午休时间': {
     id: '000-0000-000',
-    password: '000',
+    password: '',
   },
 }

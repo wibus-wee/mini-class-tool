98b411b228618abcb6baf0fe05981a39e6cd7fe5
wibus-wee <1596355173@qq.com>
2022-11-13 11:13:08 +0800

    ci: build app remove some css style

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -1,4 +1,8 @@
 rm -rf public/screen_macos_desktop_dark.jpg
 rm -rf public/screen_macos_desktop_light.jpg
 
+regex='--root-background: url\((.*)\);'
+# 删除 '--root-background'
+sed -i '' '/--root-background/d' src/main.css
+
 pnpm build
